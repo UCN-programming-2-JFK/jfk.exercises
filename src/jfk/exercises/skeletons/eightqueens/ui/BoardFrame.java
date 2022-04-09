8ba7f808fa8c1987dff74aa24bd952ee7377056d
Jakob Farian Krarup <kr@rup.dk>
2022-04-09 18:49:53 +0200

    Added solution to 8 queens

--- src/jfk/exercises/skeletons/eightqueens/ui/BoardFrame.java
+++ src/jfk/exercises/skeletons/eightqueens/ui/BoardFrame.java
@@ -6,27 +6,29 @@ import javax.swing.*;
 import jfk.exercises.skeletons.eightqueens.model.Board;
 import jfk.exercises.skeletons.maze.model.Maze;
 import jfk.exercises.skeletons.maze.tools.MazeCreator;
+import jfk.exercises.skeletons.solutions.EightQueenSolution;
 
 public class BoardFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private static BoardPanel panel;
-	private static int tileSizeInPixels = 64;
-	private static Board board = new Board();
+	private BoardPanel panel;
+	private int tileSizeInPixels = 64;
+	protected Board board = new Board();
 
 	public static void main(String[] args) {
 
-		BoardFrame window = new BoardFrame();
+		//BoardFrame window = new BoardFrame();
+		BoardFrame window = new EightQueenSolution();
 		window.setTitle("8 Queens problem solver");
 		window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		window.pack();
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
-		solve();
-		panel.repaint();
+		window.solve();
+		window.panel.repaint();
 	}
 
-	private static void solve() {
+	protected void solve() {
 		// add code to solve the 8 queens problem,
 		// and add solution (queen placement) to "board" to show it
 
